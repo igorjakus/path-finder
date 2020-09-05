6b26221ecbd14fcfc75360b16931cd6663c0c5b9
igorjakus <igorjakus3@gmail.com>
2020-09-05 13:26:38 +0200

    Created a program schema

---
+++ algorithm.py
@@ -0,0 +1,14 @@
+from scheme import Scheme
+
+class A_Star:
+
+    def __init__(self):
+        self.scheme = Scheme()
+
+    def find_path(self):
+        # Find correct shortest path
+        pass
+
+    def wrote_path(self):
+        # Wrote path in path.txt
+        pass
---
+++ path_find.py
@@ -0,0 +1,5 @@
+from algorithm import A_Star
+
+if __name__ == '__main__':
+    algo = A_Star()
+    algo.find_path()
